c3ff3bbaa875d01c78058cefa54356336ea5a3e7
Mike Yamato <mikeyamato@gmail.com>
2017-09-28 18:27:36 -0700

    download

--- Class-Content/07-firebase/01-Activities/11-countdownbutton/Unsolved/logic.js
+++ Class-Content/07-firebase/01-Activities/11-countdownbutton/Unsolved/logic.js
@@ -2,15 +2,32 @@
 // Make sure that your configuration matches your firebase script version
 // (Ex. 3.0 != 3.7.1)
 
+  var config = {
+    apiKey: "AIzaSyDqx3nnhbqeS9zVDxoAB0ykE7GjWvwEOyY",
+    authDomain: "reallyfml-26e6a.firebaseapp.com",
+    databaseURL: "https://reallyfml-26e6a.firebaseio.com",
+    projectId: "reallyfml-26e6a",
+    storageBucket: "reallyfml-26e6a.appspot.com",
+    messagingSenderId: "805719232365"
+  };
+  firebase.initializeApp(config);
+
 // Create a variable to reference the database
 // var database = ...
-
+var database = firebase.database();
 
 // Use the below initialValue
 var initialValue = 100;
 
 // Use the below variable clickCounter to keep track of the clicks.
 var clickCounter = initialValue;
+
+$("#click-button").on("click", function(){
+  clickCounter++;
+  database.ref().set({
+    clickCount: clickCounter // clickCount appears in firebase
+  });
+});
 
 // --------------------------------------------------------------
 
@@ -19,11 +36,19 @@ var clickCounter = initialValue;
 // HINT: Assuming 'database' is our database variable, use...
 // database.ref().on("value", function(snapshot)) {}
 
+database.ref().on("value", function(snapshot){
+
+
+
+
 // We are now inside our .on function...
 
 // Console.log the "snapshot" value (a point-in-time representation of the database)
+  console.log(snapshot.val());
 // This "snapshot" allows the page to get the most current values in firebase.
-
+clickCounter = snapshot.val().clickCount;
+console.log(clickCounter);
+$("#click-value").html(snapshot.val().clickCount);
 
 // Change the value of our clickCounter to match the value in the database
 // ___________ = snapshot.val().______________________
@@ -34,7 +59,9 @@ var clickCounter = initialValue;
 
 // Then include Firebase error logging
 // HINT: }, function(errorObject)
-
+}, function(errorObject){
+  console.log("The read failed: " + errorObject.code);
+});
 // --------------------------------------------------------------
 
 // Whenever a user clicks the click button
@@ -53,7 +80,9 @@ $("#click-button").on("click", function() {
   }
 
   // Save new value to Firebase
-
+  database.ref().set({
+    clickCount: clickCounter
+  });
 
   // Log the value of clickCounter
   console.log(clickCounter);
@@ -67,7 +96,9 @@ $("#restart-button").on("click", function() {
   clickCounter = initialValue;
 
   // Save new value to Firebase
-
+  database.ref().set({
+    clickCount: clickCounter
+  });
 
   // Log the value of clickCounter
   console.log(clickCounter);
